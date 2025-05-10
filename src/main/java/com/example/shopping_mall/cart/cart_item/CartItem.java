be83dcad8ff3f3212782bd1dce8782fa165dba4d
hyeblee <hyeb1224@naver.com>
2025-05-10 10:08:03 +0900

    feat: CartItem객체 생성

---
+++ src/main/java/com/example/shopping_mall/cart/cart_item/CartItem.java
@@ -0,0 +1,31 @@
+package com.example.shopping_mall.cart.cart_item;
+
+import com.example.shopping_mall.cart.Cart;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data // 게터, 세터, toString, EqualsAndHashCode, RequiredArgsContructor 자동 생성
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItem {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int cartItemId;
+
+//  @ManyToOne
+//  private Cart cart;
+//
+//  @ManyToOne
+//  private Product product;
+
+}
---
+++ src/main/java/com/example/shopping_mall/cart/cart_item/CartItemRepository.java
@@ -0,0 +1,7 @@
+package com.example.shopping_mall.cart.cart_item;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+}
