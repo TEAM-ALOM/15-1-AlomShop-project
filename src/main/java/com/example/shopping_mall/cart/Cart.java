669ca6313e75036ad5d24f39e939897952f40bb8
hyeblee <hyeb1224@naver.com>
2025-05-10 10:06:13 +0900

    feat: Cart객체 생성

---
+++ src/main/java/com/example/shopping_mall/cart/Cart.java
@@ -0,0 +1,26 @@
+package com.example.shopping_mall.cart;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cart {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int cartId;
+
+//  @OneToOne
+//  User user;
+}
