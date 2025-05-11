b7d53bf9c9fdcef80539076527db715d371d5966
jaykwon0207 <zoidshj0119@gmail.com>
2025-05-11 13:55:32 +0900

    fix: 주문 필터링 조건 수정

--- src/main/java/com/example/shopping_mall/order/OrderController.java
+++ src/main/java/com/example/shopping_mall/order/OrderController.java
@@ -16,17 +16,17 @@ public class OrderController {
 
     private final OrderService orderService;
 
-//    @PostMapping
-//    public ResponseEntity<String> createOrder(@RequestBody OrderRequestDto dto) {
-//        orderService.createOrder(dto.getUserId(), dto.getProductId(),dto.getOrderDate(),dto.getQuantity());
-//        return ResponseEntity.ok("주문이 완료되었습니다.");
-//    }
-//
-//    @GetMapping
-//    public ResponseEntity<List<Order>> getOrders(@ModelAttribute OrderSearchRequestDto dto){
-//        List<Order> orders = orderService.findOrder(dto);
-//        return ResponseEntity.ok(orders);
-//    }
+    @PostMapping
+    public ResponseEntity<String> createOrder(@RequestBody OrderRequestDto dto) {
+        orderService.createOrder(dto.getUserId(), dto.getProductId(),dto.getOrderDate(),dto.getQuantity());
+        return ResponseEntity.ok("주문이 완료되었습니다.");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Order>> getOrders(@ModelAttribute OrderSearchRequestDto dto){
+        List<Order> orders = orderService.findOrder(dto);
+        return ResponseEntity.ok(orders);
+    }
 
     @DeleteMapping
     public ResponseEntity<String> deleteOrder(@RequestParam Long orderId) {
